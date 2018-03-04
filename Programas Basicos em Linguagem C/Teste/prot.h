e7820d1eff5e57d0e64309f0f7e446b55a07268a
Roberto <r_t_4@hotmail.com>
2018-03-04 15:39:40 -0300

    cg

---
+++ Programas Basicos em Linguagem C/Teste/prot.h
@@ -0,0 +1,9 @@
+#ifndef PROT /** Evitar duplicação de código. */
+
+/* Protótipos das funções, logo abaixo. */
+
+extern void mansagem(); /** extern indica que esta função estará em alguma parte do programa. */
+extern int soma(int,int);
+extern void exiba();
+
+#endif // PROT
